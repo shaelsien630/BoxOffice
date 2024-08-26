be949fa7ee1cd4170f1a3881ff96b80f4ff7fea7
shaelsien630 <shaelsien630@gmail.com>
2024-08-26 13:43:59 +0900

    Initial Commit

---
+++ BoxOffice/BoxOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxOfficeApp.swift
+//  BoxOffice
+//
+//  Created by 최서희 on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BoxOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
